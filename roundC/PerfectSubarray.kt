79fd60097805e6ef71a41f9e2de782babaa4ffa0
Shutarou Takayama <hi.mountain110916@gmail.com>
2020-05-18 16:17:29 +0900

    忘れ物

--- roundC/PerfectSubarray.kt
+++ roundC/PerfectSubarray.kt
@@ -1,4 +1,4 @@
-import kotlin.math.max
+import kotlin.math.*
 
 fun main(args: Array<String>) {
     val T = readLine()!!.toInt()
@@ -9,16 +9,16 @@ fun main(args: Array<String>) {
         list.forEach{a ->
             max_val += max(a, 0)
         }
-        var squares = mutableSetOf()
+        var squares:MutableSet<Int> = mutableSetOf()
         var i = 0
-        while(i ** 2) {
-            squares.add(i ** 2)
+        while(i * i <= max_val) {
+            squares.add(i * i)
             i += 1
         }
         var cnt = 0
         for (i in 0..N-1) {
             var cum = 0
-            list.slice(i..).forEach{
+            list.slice(i..N-1).forEach{
                 cum += it
                 if (cum in squares) {
                     cnt += 1
